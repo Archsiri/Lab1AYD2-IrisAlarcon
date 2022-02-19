565423d8b7be1fc298b93f015816ca3baf7674d7
Archsiri <irisc.alarconp@gmail.com>
2022-02-19 17:02:13 -0600

    Actualizacion de servicio de API con metodos get, post y delete

--- src/app/services/reqresapi.service.ts
+++ src/app/services/reqresapi.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReqresapiService {
 
-  constructor() { }
+  private uri = 'https://reqres.in/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getAllUsers(){
+    return this.http.get('https://reqres.in/api/users?page=2');
+  }
+
+  getUser(idUser: number){
+    return this.http.get(this.uri+'users/'+idUser)
+  }
+
+  createUser(userName: string, userJob: string){
+    let user_data = {
+      name: userName,
+      job: userJob
+    }
+    return this.http.post(this.uri+'users', user_data)
+  }
+
+  updateUser(){}
+
+  deleteUser(userId: number){
+    this.http.delete(this.uri+'users/'+userId)
+  }
 }
